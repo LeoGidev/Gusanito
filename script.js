a52e35a92e31f3a6f5ba5918843854dbc4470abe
LeoGidev <leogimenez.dev@gmail.com>
2024-03-25 20:45:35 -0300

    se trabaja en el js

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
     const canvas = document.getElementById('gameCanvas');
     const ctx = canvas.getContext('2d');
-    
+    const boxSize = 20;
+  const gridSize = canvas.width / boxSize;
+  let snake = [{ x: 10, y: 10 }];
+  let food = generateFood();
+
+  let dx = 0;
+  let dy = 0;
+  let score = 0;
+}///
